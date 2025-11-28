5d2dbd28485e7d846d332443ff491a00082b17b7
tirumalateja19 <tirumalateja.jampani@gmail.com>
2025-11-28 11:37:31 +0530

    Request Reviewer API

--- src/routes/request.js
+++ src/routes/request.js
@@ -51,4 +51,35 @@ requestRouter.get(
   }
 );
 
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+      const statusAllowed = ["accepted", "rejected"];
+      if (!statusAllowed.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed!" });
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        status: "interested",
+        toUserId: loggedInUser._id,
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found!" });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.status(200).json({ message: "Connection request " + status, data });
+    } catch (err) {
+      res.status(400).send("Error " + err.message);
+    }
+  }
+);
+
 module.exports = requestRouter;
