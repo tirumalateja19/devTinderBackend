fec4bda85185c342e2e8854a5117fc22cbca4061
tirumalateja19 <tirumalateja.jampani@gmail.com>
2026-05-04 10:16:10 +0530

    Deplyment changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 // const { DB_URL } = require("./constants");
 const DB_URL = process.env.DB_URL;
-console.log("DB_URL:", process.env.DB_URL);
 const connectDB = async () => {
   await mongoose.connect(DB_URL);
 };
