388d8c6883dd846033f25c46ba27ff13a13e0e04
tirumalateja19 <tirumalateja.jampani@gmail.com>
2026-05-04 10:26:41 +0530

    deployment changes_2

--- src/server.js
+++ src/server.js
@@ -11,12 +11,7 @@ require("dotenv").config();
 
 const cors = require("cors");
 const PORT = process.env.PORT;
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
